6c3a624acc4c9cef62207b66156056d1e4e5355d
sainadha-reddy3 <sainathreddykasina@gmail.com>
2026-07-19 20:44:58 +0530

    Fix app.py: remove Diabetes section, add trained models to Models folder

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Load the saved models
 models = {
-    'diabetes': pickle.load(open('Models/diabetes_model.sav', 'rb')),
     'heart_disease': pickle.load(open('Models/heart_disease_model.sav', 'rb')),
     'parkinsons': pickle.load(open('Models/parkinsons_model.sav', 'rb')),
     'lung_cancer': pickle.load(open('Models/lungs_disease_model.sav', 'rb')),
@@ -53,8 +52,7 @@ models = {
 # Create a dropdown menu for disease prediction
 selected = st.selectbox(
     'Select a Disease to Predict',
-    ['Diabetes Prediction',
-     'Heart Disease Prediction',
+    ['Heart Disease Prediction',
      'Parkinsons Prediction',
      'Lung Cancer Prediction',
      'Hypo-Thyroid Prediction']
@@ -65,26 +63,6 @@ def display_input(label, tooltip, key, type="text"):
         return st.text_input(label, key=key, help=tooltip)
     elif type == "number":
         return st.number_input(label, key=key, help=tooltip, step=1)
-
-# Diabetes Prediction Page
-if selected == 'Diabetes Prediction':
-    st.title('Diabetes')
-    st.write("Enter the following details to predict diabetes:")
-
-    Pregnancies = display_input('Number of Pregnancies', 'Enter number of times pregnant', 'Pregnancies', 'number')
-    Glucose = display_input('Glucose Level', 'Enter glucose level', 'Glucose', 'number')
-    BloodPressure = display_input('Blood Pressure value', 'Enter blood pressure value', 'BloodPressure', 'number')
-    SkinThickness = display_input('Skin Thickness value', 'Enter skin thickness value', 'SkinThickness', 'number')
-    Insulin = display_input('Insulin Level', 'Enter insulin level', 'Insulin', 'number')
-    BMI = display_input('BMI value', 'Enter Body Mass Index value', 'BMI', 'number')
-    DiabetesPedigreeFunction = display_input('Diabetes Pedigree Function value', 'Enter diabetes pedigree function value', 'DiabetesPedigreeFunction', 'number')
-    Age = display_input('Age of the Person', 'Enter age of the person', 'Age', 'number')
-
-    diab_diagnosis = ''
-    if st.button('Diabetes Test Result'):
-        diab_prediction = models['diabetes'].predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
-        diab_diagnosis = 'The person is diabetic' if diab_prediction[0] == 1 else 'The person is not diabetic'
-        st.success(diab_diagnosis)
 
 # Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
